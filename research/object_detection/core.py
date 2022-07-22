a08fedf616db6eb6f5fe30d6e7f48a7dc05f034c
Alexandre Leobons Souza <aleobons@gmail.com>
2022-07-22 15:56:57 +0000

    inclusão de tracking e validation com o treino

---
+++ research/object_detection/core.py
@@ -0,0 +1,175 @@
+from pathlib import Path
+import re
+import os
+from configparser import ConfigParser
+from typing import List, Dict
+
+from pydantic import BaseModel, constr, validator
+from strictyaml import load, YAML
+
+from object_detection.utils import config_util
+
+
+# instantiate
+config = ConfigParser()
+# parse existing file
+# config.read("az_tf_od_api.ini")
+
+# CONFIG_FILE_PATH = Path(config.get("config_files", "config"))
+
+losses_allowed = [
+    "Loss/total_loss",
+    "Loss/localization_loss",
+    "Loss/regularization_loss",
+    "Loss/classification_loss",
+]
+
+metrics_allowed = [
+    "DetectionBoxes_Precision/mAP",
+    "DetectionBoxes_Precision/mAP (large)",
+    "DetectionBoxes_Precision/mAP (medium)",
+    "DetectionBoxes_Precision/mAP (small)",
+    "DetectionBoxes_Precision/mAP@.50IOU",
+    "DetectionBoxes_Precision/mAP@.75IOU",
+    "DetectionBoxes_Recall/AR@1",
+    "DetectionBoxes_Recall/AR@10",
+    "DetectionBoxes_Recall/AR@100",
+    "DetectionBoxes_Recall/AR@100 (large)",
+    "DetectionBoxes_Recall/AR@100 (medium)",
+    "DetectionBoxes_Recall/AR@100 (small)",
+]
+
+
+def path_pipeline_config_validator(path_pipeline_config: str) -> str:
+
+    if re.match(
+        "^\./([a-z0-9\-\_]+/)*[a-zA-Z0-9\-\_]+\.config$",
+        path_pipeline_config,
+    ):
+        return path_pipeline_config
+
+    raise ValueError(f"Invalid path pipeline config: {path_pipeline_config}")
+
+
+class TrackingConfig(BaseModel):
+    """
+    Tracking-level config.
+    """
+
+    # the order is necessary for validation
+    losses_allowed: List[str]
+    metrics_allowed: List[str]
+
+    path_pipeline_config: str
+    _validator_path_pipeline_config = validator(
+        "path_pipeline_config", pre=True, allow_reuse=True
+    )(path_pipeline_config_validator)
+
+    parametros: List[
+        constr(regex="^[a-z][a-z0-9\-\_]*(\.[a-z0-9\-\_]*)+([a-z0-9\-\_])+$")
+    ]
+    metricas: Dict[str, str]
+    losses: Dict[str, str]
+
+    # TODO - checar se a métrica existe
+    metrica_para_avaliacao: str
+
+    @validator("parametros")
+    def validate_parametros(cls, params, values):
+        def _validate_message_has_field(message, field):
+            if not message.HasField(field):
+                raise ValueError("Expecting message to have field %s" % field)
+
+        path_pipeline_config = Path(values.get("path_pipeline_config"))
+        if not os.path.exists(path_pipeline_config):
+            raise ValueError(f"Path pipeline config not found: {path_pipeline_config}")
+
+        pipeline_config = config_util.get_configs_from_pipeline_file(
+            path_pipeline_config
+        )
+
+        for param in params:
+            fields = param.split(".")
+            first_field = fields.pop(0)
+            message = pipeline_config[first_field]
+
+            for field in fields:
+                _validate_message_has_field(message, field)
+                message = getattr(message, field)
+
+        return params
+
+    @validator("losses")
+    def allowed_losses(cls, losses, values):
+
+        allowed_loss = values.get("losses_allowed")
+
+        for loss in losses.values():
+            if loss not in allowed_loss:
+                raise ValueError(
+                    f"the loss parameter specified: {loss}, "
+                    f"is not in the allowed set: {allowed_loss}"
+                )
+        return losses
+
+    @validator("metricas")
+    def allowed_metrics(cls, metrics, values):
+
+        allowed_metrics = values.get("metrics_allowed")
+
+        for metric in metrics.values():
+            if metric not in allowed_metrics:
+                raise ValueError(
+                    f"the metric parameter specified: {metric}, "
+                    f"is not in the allowed set: {allowed_metrics}"
+                )
+        return metrics
+
+
+class Config(BaseModel):
+    """Master config object."""
+
+    tracking_config: TrackingConfig
+
+
+# def find_config_file() -> Path:
+#     """Locate the configuration file."""
+#     if CONFIG_FILE_PATH.is_file():
+#         return CONFIG_FILE_PATH
+#     raise Exception(f"Config not found at {CONFIG_FILE_PATH!r}")
+
+
+def fetch_config_from_yaml(cfg_path: Path) -> YAML:
+    """Parse YAML containing the package configuration."""
+
+    # if not cfg_path:
+    #     cfg_path = find_config_file()
+
+    if os.path.exists(cfg_path):
+        with open(cfg_path, "r") as conf_file:
+            parsed_config = load(conf_file.read())
+            return parsed_config
+    raise OSError(f"Did not find config file at path: {cfg_path}")
+
+
+def create_and_validate_config(
+    parsed_config_path: Path, parsed_config: YAML = None
+) -> Config:
+    """Run validation on config values."""
+    if parsed_config is None:
+        parsed_config = fetch_config_from_yaml(parsed_config_path)
+
+    parsed_config_dict = parsed_config.data
+    parsed_config_dict.update(
+        {"losses_allowed": losses_allowed, "metrics_allowed": metrics_allowed}
+    )
+
+    # specify the data attribute from the strictyaml YAML type.
+    _config = Config(
+        tracking_config=TrackingConfig(**parsed_config_dict),
+    )
+
+    return _config
+
+
+# config = create_and_validate_config()
--- research/object_detection/model_main_tf2.py
+++ research/object_detection/model_main_tf2.py
@@ -1,3 +1,4 @@
+# Lint as: python3
 # Copyright 2020 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,89 +27,204 @@ python model_main_tf2.py -- \
   --pipeline_config_path=$PIPELINE_CONFIG_PATH \
   --alsologtostderr
 """
+import json
+import os
 from absl import flags
 import tensorflow.compat.v2 as tf
+import tensorflow.compat.v1 as tf1
+
 from object_detection import model_lib_v2
+from object_detection import core as config
 
-flags.DEFINE_string('pipeline_config_path', None, 'Path to pipeline config '
-                    'file.')
-flags.DEFINE_integer('num_train_steps', None, 'Number of train steps.')
-flags.DEFINE_bool('eval_on_train_data', False, 'Enable evaluating on train '
-                  'data (only supported in distributed training).')
-flags.DEFINE_integer('sample_1_of_n_eval_examples', None, 'Will sample one of '
-                     'every n eval input examples, where n is provided.')
-flags.DEFINE_integer('sample_1_of_n_eval_on_train_examples', 5, 'Will sample '
-                     'one of every n train input examples for evaluation, '
-                     'where n is provided. This is only used if '
-                     '`eval_training_data` is True.')
-flags.DEFINE_string(
-    'model_dir', None, 'Path to output model directory '
-                       'where event and checkpoint files will be written.')
-flags.DEFINE_string(
-    'checkpoint_dir', None, 'Path to directory holding a checkpoint.  If '
-    '`checkpoint_dir` is provided, this binary operates in eval-only mode, '
-    'writing resulting metrics to `model_dir`.')
 
-flags.DEFINE_integer('eval_timeout', 3600, 'Number of seconds to wait for an'
-                     'evaluation checkpoint before exiting.')
+tf.get_logger().setLevel("ERROR")
 
-flags.DEFINE_bool('use_tpu', False, 'Whether the job is executing on a TPU.')
-flags.DEFINE_string(
-    'tpu_name',
-    default=None,
-    help='Name of the Cloud TPU for Cluster Resolvers.')
+# Enable GPU dynamic memory allocation
+gpus = tf.config.experimental.list_physical_devices("GPU")
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+flags.DEFINE_string("pipeline_config_path", None, "Path to pipeline config " "file.")
+flags.DEFINE_integer("num_train_steps", None, "Number of train steps.")
+flags.DEFINE_bool(
+    "eval_on_train_data",
+    False,
+    "Enable evaluating on train " "data (only supported in distributed training).",
+)
 flags.DEFINE_integer(
-    'num_workers', 1, 'When num_workers > 1, training uses '
-    'MultiWorkerMirroredStrategy. When num_workers = 1 it uses '
-    'MirroredStrategy.')
+    "sample_1_of_n_eval_examples",
+    None,
+    "Will sample one of " "every n eval input examples, where n is provided.",
+)
 flags.DEFINE_integer(
-    'checkpoint_every_n', 1000, 'Integer defining how often we checkpoint.')
-flags.DEFINE_boolean('record_summaries', True,
-                     ('Whether or not to record summaries defined by the model'
-                      ' or the training pipeline. This does not impact the'
-                      ' summaries of the loss values which are always'
-                      ' recorded.'))
+    "sample_1_of_n_eval_on_train_examples",
+    5,
+    "Will sample "
+    "one of every n train input examples for evaluation, "
+    "where n is provided. This is only used if "
+    "`eval_training_data` is True.",
+)
+flags.DEFINE_string(
+    "model_dir",
+    None,
+    "Path to output model directory "
+    "where event and checkpoint files will be written.",
+)
+flags.DEFINE_string(
+    "checkpoint_dir",
+    None,
+    "Path to directory holding a checkpoint.  If "
+    "`checkpoint_dir` is provided, this binary operates in eval-only mode, "
+    "writing resulting metrics to `model_dir`.",
+)
+
+flags.DEFINE_integer(
+    "eval_timeout",
+    3600,
+    "Number of seconds to wait for an" "evaluation checkpoint before exiting.",
+)
+
+flags.DEFINE_bool("use_tpu", False, "Whether the job is executing on a TPU.")
+flags.DEFINE_string(
+    "tpu_name", default=None, help="Name of the Cloud TPU for Cluster Resolvers."
+)
+flags.DEFINE_integer(
+    "num_workers",
+    1,
+    "When num_workers > 1, training uses "
+    "MultiWorkerMirroredStrategy. When num_workers = 1 it uses "
+    "MirroredStrategy.",
+)
+flags.DEFINE_integer(
+    "checkpoint_every_n", 1000, "Integer defining how often we checkpoint."
+)
+flags.DEFINE_boolean(
+    "record_summaries",
+    True,
+    (
+        "Whether or not to record summaries defined by the model"
+        " or the training pipeline. This does not impact the"
+        " summaries of the loss values which are always"
+        " recorded."
+    ),
+)
+flags.DEFINE_integer(
+    "checkpoint_max_to_keep",
+    7,
+    ("the number of most recent checkpoints to keep in the model directory"),
+)
+
+flags.DEFINE_string("summary_file_path", None, ("tensorboard logs location"))
+flags.DEFINE_string(
+    "pipeline_config_override_path", None, "Path to pipeline config override file"
+)
+
+flags.DEFINE_string("input_train", None, "Path to input train data")
+flags.DEFINE_string("input_train_pattern", "", "Pattern for input train files")
+
+flags.DEFINE_string("input_val", None, "Path to input val data")
+flags.DEFINE_string("input_val_pattern", "", "Pattern for input train files")
+
+flags.DEFINE_string(
+    "input_model_type",
+    "encoded_image_string_tensor",
+    "Type of input data in score model",
+)
+flags.DEFINE_string("model_output", "model", "Path to model output")
+
+flags.DEFINE_string("tracking_config_path", None, "Path to tracking config")
+
 
 FLAGS = flags.FLAGS
 
 
 def main(unused_argv):
-  flags.mark_flag_as_required('model_dir')
-  flags.mark_flag_as_required('pipeline_config_path')
-  tf.config.set_soft_device_placement(True)
+    flags.mark_flag_as_required("model_dir")
+    flags.mark_flag_as_required("pipeline_config_path")
+    flags.mark_flag_as_required("input_train")
+    flags.mark_flag_as_required("input_val")
+    flags.mark_flag_as_required("model_output")
 
-  if FLAGS.checkpoint_dir:
-    model_lib_v2.eval_continuously(
-        pipeline_config_path=FLAGS.pipeline_config_path,
-        model_dir=FLAGS.model_dir,
-        train_steps=FLAGS.num_train_steps,
-        sample_1_of_n_eval_examples=FLAGS.sample_1_of_n_eval_examples,
-        sample_1_of_n_eval_on_train_examples=(
-            FLAGS.sample_1_of_n_eval_on_train_examples),
-        checkpoint_dir=FLAGS.checkpoint_dir,
-        wait_interval=300, timeout=FLAGS.eval_timeout)
-  else:
-    if FLAGS.use_tpu:
-      # TPU is automatically inferred if tpu_name is None and
-      # we are running under cloud ai-platform.
-      resolver = tf.distribute.cluster_resolver.TPUClusterResolver(
-          FLAGS.tpu_name)
-      tf.config.experimental_connect_to_cluster(resolver)
-      tf.tpu.experimental.initialize_tpu_system(resolver)
-      strategy = tf.distribute.experimental.TPUStrategy(resolver)
-    elif FLAGS.num_workers > 1:
-      strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+    tf.config.set_soft_device_placement(True)
+
+    tracking_config = None
+    if FLAGS.tracking_config_path is not None:
+        tracking_config = config.create_and_validate_config(
+            parsed_config_path=FLAGS.tracking_config_path
+        )
+
+    if FLAGS.checkpoint_dir:
+        if FLAGS.pipeline_config_override_path:
+            with tf1.gfile.GFile(FLAGS.pipeline_config_override_path, "r") as f:
+                config_override = f.read()
+        else:
+            config_override = None
+
+        model_lib_v2.eval_continuously(
+            pipeline_config_path=FLAGS.pipeline_config_path,
+            model_dir=FLAGS.model_dir,
+            train_steps=FLAGS.num_train_steps,
+            sample_1_of_n_eval_examples=FLAGS.sample_1_of_n_eval_examples,
+            sample_1_of_n_eval_on_train_examples=(
+                FLAGS.sample_1_of_n_eval_on_train_examples
+            ),
+            checkpoint_dir=FLAGS.checkpoint_dir,
+            wait_interval=300,
+            timeout=FLAGS.eval_timeout,
+            use_tpu=FLAGS.use_tpu,
+            summary_file_path=FLAGS.summary_file_path,
+            config_override=config_override,
+        )
     else:
-      strategy = tf.compat.v2.distribute.MirroredStrategy()
+        if FLAGS.use_tpu:
+            # TPU is automatically inferred if tpu_name is None and
+            # we are running under cloud ai-platform.
+            print("[INFO] using TPU...")
+            resolver = tf.distribute.cluster_resolver.TPUClusterResolver(FLAGS.tpu_name)
+            tf.config.experimental_connect_to_cluster(resolver)
+            tf.tpu.experimental.initialize_tpu_system(resolver)
+            strategy = tf.distribute.TPUStrategy(resolver)
+        elif FLAGS.num_workers > 1:
+            print("[INFO] using MultiWorkerMirroredStrategy...")
+            strategy = tf.distribute.MultiWorkerMirroredStrategy()
+        else:
+            strategy = tf.compat.v2.distribute.MirroredStrategy()
 
-    with strategy.scope():
-      model_lib_v2.train_loop(
-          pipeline_config_path=FLAGS.pipeline_config_path,
-          model_dir=FLAGS.model_dir,
-          train_steps=FLAGS.num_train_steps,
-          use_tpu=FLAGS.use_tpu,
-          checkpoint_every_n=FLAGS.checkpoint_every_n,
-          record_summaries=FLAGS.record_summaries)
+        def export(data, _):
+            with open("outputs/metrics.json", "w") as f:
+                json.dump(data, f)
 
-if __name__ == '__main__':
-  tf.compat.v1.app.run()
+        input_train = (
+            os.path.join(FLAGS.input_train, FLAGS.input_train_pattern)
+            if os.path.isdir(FLAGS.input_train)
+            else FLAGS.input_train
+        )
+
+        input_val = (
+            os.path.join(FLAGS.input_val, FLAGS.input_val_pattern)
+            if os.path.isdir(FLAGS.input_val)
+            else FLAGS.input_val
+        )
+
+        with strategy.scope():
+            model_lib_v2.train_loop(
+                pipeline_config_path=FLAGS.pipeline_config_path,
+                model_dir=FLAGS.model_dir,
+                input_model_type=FLAGS.input_model_type,
+                model_output=FLAGS.model_output,
+                train_steps=FLAGS.num_train_steps,
+                use_tpu=FLAGS.use_tpu,
+                checkpoint_every_n=FLAGS.checkpoint_every_n,
+                record_summaries=FLAGS.record_summaries,
+                checkpoint_max_to_keep=FLAGS.checkpoint_max_to_keep,
+                summary_file_path=FLAGS.summary_file_path,
+                performance_summary_exporter=export,
+                input_train=input_train,
+                input_val=input_val,
+                pipeline_config_override_path=FLAGS.pipeline_config_override_path,
+                tracking_config=tracking_config,
+            )
+
+
+if __name__ == "__main__":
+    tf.compat.v1.app.run()
